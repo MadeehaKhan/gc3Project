586be5ed160d6e9ef60d17dfaa468088ff351c30
Noshin Bari <noshint@mcmaster.ca>
2019-04-09 14:00:58 -0400

    working version

---
+++ Tangram/Assets/ScriptLing/background.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public float scrollspeed = -2f;
+	public float offset;
+	Vector2 sPos;
+	float newPos;
+    void Start()
+    {
+		sPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		newPos = Mathf.Repeat(Time.time*scrollspeed, offset);
+		transform.position = sPos + Vector2.right*newPos;
+    }
+}
